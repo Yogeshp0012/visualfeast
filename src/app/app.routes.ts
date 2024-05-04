d7cb22aa46b6caa7c1416cd0cf2d3f76aec70246
Yogeshp0012 <yogeshp0012@gmail.com>
2024-05-04 14:06:01 +0530

    Initial Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EditImageComponent } from './edit-image/edit-image.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "edit", component: EditImageComponent},
+    {path: "home", component: HomeComponent},
+    {path: "**", redirectTo: "home"}
+];
