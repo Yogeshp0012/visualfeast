15d4752f718f71396f377a19d050b1aa9c0d1d47
Yogeshp0012 <yogeshp0012@gmail.com>
2024-05-07 10:55:05 +0530

    Fix images

--- src/app/image-card/image-card.component.ts
+++ src/app/image-card/image-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-image-card',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './image-card.component.html',
   styleUrl: './image-card.component.scss'
 })
